e30afee166fbeb79ae04c2be0a18308b6ce63a22
wankoutecto <stevewankou@yahoo.fr>
2026-07-06 23:53:55 -0400

    Initial commit

---
+++ airline-backend/src/main/java/com/example/airline_booking_system/AirlineBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.airline_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineBookingSystemApplication.class, args);
+	}
+
+}
---
+++ airline-backend/src/test/java/com/example/airline_booking_system/AirlineBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airline_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
